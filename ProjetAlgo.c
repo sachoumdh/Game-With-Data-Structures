4daaabf11e43524946ca4da65126d95363faf5f8
Champily <Lynadjenkal@gmail.com>
2025-12-21 17:33:54 +0100

    Update ProjetAlgo.c

--- ProjetAlgo.c
+++ ProjetAlgo.c
@@ -5,7 +5,7 @@
 typedef struct {
     int num, age;
     char nom[20];
-    int score_partie;
+    int score_partie , points_succesifs;
     int nb_victoires, nb_defaites, nb_victoires_succ , nb_defaites_succ;
     bool is_winner;
 } Joueur;
