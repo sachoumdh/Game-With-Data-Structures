32ea30a620315f364e30dadc6b7f44db066e2517
Champily <Lynadjenkal@gmail.com>
2025-12-21 16:32:10 +0100

    Update ProjetAlgo.c

--- ProjetAlgo.c
+++ ProjetAlgo.c
@@ -1,20 +1,35 @@
 #include <stdio.h>
 
+//record Joueur
 typedef struct {
     int num, age;
     char nom[20];
     int nb_victoires, nb_defaites;
 } Joueur;
 
+//Record Partie
 typedef struct {
     int num_partie;
     char nom_J1 , nom_J2 [20];
     int score_J1 , score_J2;
 }partie;
+
+//Structure P pour stocker les parties 
 typedef struct {
     partie round;
     struct P *Next ;
 } P;
+
+//Node est l'element de la file
+typedef struct {
+    Joueur J;
+    struct Node *Jnext;
+} Node;
+//structure File
+typedef struct {
+    Node *Head;
+    Node *Tail;
+}File
 
 int main(int argc, const char * argv[]) {
     // insert code here...
